6f59bbc705c1dd125ceeb637a41ea817bc1e1fe2
aimy <aimy.app@gmail.com>
2020-06-30 22:49:18 +0900

    楽天市場の枠作成

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      実機ちゃん
-    </v-app-bar>
+  <div>
+    <v-app>
+      <v-app-bar app>
+        実機ちゃん
+      </v-app-bar>
 
-    <!-- Sizes your content based upon application components -->
-    <v-main>
+      <!-- Sizes your content based upon application components -->
+      <v-main>
 
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          
+          <!-- If using vue-router -->
+          <br><br><br><br>
+          <Rakuten></Rakuten>
+        </v-container>
+      </v-main>
 
-  
-    </v-main>
-
-    <v-footer app>
-      実機ちゃん
-    </v-footer>
-    </v-app>
+      <v-footer app>
+        実機ちゃん
+      </v-footer>
+    </v-app> 
+  </div>
 </template>
 
 <script>
+import Rakuten from './components/Rakuten.vue'
+export default {
+  components: {
+    Rakuten
+  }
+}
 
 </script>
---
+++ src/components/Rakuten.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="rakuten">
+        <h2>{{ title }}</h2>
+            <div class="rakutenCard" v-for="zikki in zikkiList" :key="zikki.id">
+                {{ zikki.name }}
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: "楽天市場",
+            zikkiList: [
+                { id: 1, name: "HANABI"},
+                { id: 2, name: "VERSUS"},
+                { id: 3, name: "サンダーvリボルト"}
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.rakuten {
+    border: 1px solid gray
+}
+
+.rakutenCard {
+    display: inline-block;
+    
+}
+</style>
