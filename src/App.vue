0b42d531a5fefe4c5fb5001dc67b2d4434f82953
aimy <aimy.app@gmail.com>
2020-07-05 03:13:32 +0900

    楽天市場の枠作成続き

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <div>
+  
     <v-app>
-      <v-app-bar app>
-        実機ちゃん
+      <v-app-bar app dark>
+        サイト名
       </v-app-bar>
-
-      <!-- Sizes your content based upon application components -->
-      <v-main>
-
-        <!-- Provides the application the proper gutter -->
-        <v-container fluid>
-          
-          <!-- If using vue-router -->
-          <br><br><br><br>
+          <br><br><br>
           <Rakuten></Rakuten>
-        </v-container>
-      </v-main>
-
-      <v-footer app>
-        実機ちゃん
-      </v-footer>
     </v-app> 
-  </div>
+  
 </template>
 
 <script>
--- src/components/Rakuten.vue
+++ src/components/Rakuten.vue
@@ -1,34 +1,47 @@
 <template>
-    <div class="rakuten">
-        <h2>{{ title }}</h2>
-            <div class="rakutenCard" v-for="zikki in zikkiList" :key="zikki.id">
-                {{ zikki.name }}
-            </div>
-    </div>
+  <v-container class="grey lighten-4" fluid>
+    <v-card-title>{{ shopName }}</v-card-title>
+    <v-row>
+      <v-col v-for="zikki in zikkis" :key="zikki" cols="12" sm="6" md="4">
+        <v-card>
+          <v-img
+            :src="`https://upload.wikimedia.org/wikipedia/commons/0/0e/No_image.jpg`"
+            height="300px"
+            width="200px"
+          ></v-img>
+          <v-card-subtitle class="pb-0">機種名: {{ zikki }}</v-card-subtitle>
+          <v-card-text class="text--primary">
+            <div style="color:red">00,000円 (送料別)</div>
+
+            <div>説明文</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="orange" text>購入ページへ</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            title: "楽天市場",
-            zikkiList: [
-                { id: 1, name: "HANABI"},
-                { id: 2, name: "VERSUS"},
-                { id: 3, name: "サンダーvリボルト"}
-            ]
-        }
-    }
-}
-</script>
+  data: () => ({
+    shopName: "楽天市場",
+    zikkis: [
+      "HANABI",
+      "VERSUS",
+      "サンダーVリボルト",
+      "B-MAX",
+      "クランキーコレクション",
+      "ゲッターマウス",
+      "アレックス",
+      "ドンちゃん2",
+      "サンダーVライトニング"
+    ]
+  })
+};
+</script> 
 
 <style scoped>
-.rakuten {
-    border: 1px solid gray
-}
-
-.rakutenCard {
-    display: inline-block;
-    
-}
 </style>
